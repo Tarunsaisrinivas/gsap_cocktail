1e3e59b71ee83ee3d409cefd4cd68531f8b99360
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-09-24 07:23:52 +0530

    Hero Section: Title and leafs are arranged

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return <main>
     <Navbar />
+    <Hero />
   </main>;
 };
 
